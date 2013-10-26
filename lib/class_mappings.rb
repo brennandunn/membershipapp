5d37fc86fc684a8e5a59ebdf1bca0231093b4034
Brennan Dunn <me@brennandunn.com>
2013-10-26 08:55:04 -0400

    Add new class

--- lib/class_mappings.rb
+++ lib/class_mappings.rb
@@ -9,7 +9,8 @@ class ClassMappings
 		'Fam1013' => 207,
 		'Cons1013' => 209,
 		'Agen1013' => 211,
-		'Grow1013' => 213
+		'Grow1013' => 213,
+		'Emerge1013' => 219
 	}
 
 	def self.for_cohort(cohort)
