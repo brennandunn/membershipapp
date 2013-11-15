afa1ab10f1a7ec580454de37295a2fae7879e045
Brennan Dunn <me@brennandunn.com>
2013-11-14 19:08:42 -0500

    Add class mappings

--- lib/class_mappings.rb
+++ lib/class_mappings.rb
@@ -11,7 +11,8 @@ class ClassMappings
 		'Agen1013' => 211,
 		'Grow1013' => 213,
 		'Emerge1013' => 219,
-		'Grow1113' => 225
+		'Grow1113' => 225,
+		'Emerge1113' => 227
 	}
 
 	def self.for_cohort(cohort)
